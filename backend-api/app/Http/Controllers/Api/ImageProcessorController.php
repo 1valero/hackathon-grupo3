ea1f72bd81519b22f02072f8f74964294820ce63
David de la Cruz <daviddlcruz@outlook.com>
2021-08-07 08:42:58 -0500

    Integracion de google cloud storage

--- backend-api/app/Http/Controllers/Api/ImageProcessorController.php
+++ backend-api/app/Http/Controllers/Api/ImageProcessorController.php
@@ -3,17 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Google\Cloud\Storage\StorageClient;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 
 class ImageProcessorController extends Controller
 {
+    const BASE_GCS_ENDPOINT = "https://storage.googleapis.com/";
+
     public function upload(Request $request)
     {
-        $result = $request->file('photo')->store('api/photos');
+        $filePath = $request->file('photo')->storePublicly('user_001','gcs');
 
         return response()->json([
-            "results" => $result,
+            "results" => [
+                "photo_url" => self::BASE_GCS_ENDPOINT . config('filesystems.disks.gcs.bucket') . DIRECTORY_SEPARATOR . $filePath,
+            ],
             "status" => "OK"
         ], Response::HTTP_OK);
     }
